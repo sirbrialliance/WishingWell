4812f5bcce5e7c642a54956dc7e4f66416539457
vazor222 <vazor222@gmail.com>
2015-10-09 22:08:01 -0600

    readme scenes update, main scene added, first fallcontroller attempt added

---
+++ Assets/Scripts/FallController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallController : MonoBehaviour {
+
+    public int wellTop = 0;
+    public int wellBottom = -5000;
+    public Rigidbody[] fallers;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    foreach( Rigidbody r in fallers )
+        {
+            if( r.position.y < wellBottom )
+            {
+                r.MovePosition(new Vector3(r.position.x, wellTop, r.position.z));
+            }
+        }
+	}
+}
