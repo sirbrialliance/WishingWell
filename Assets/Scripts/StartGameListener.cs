656907cc8fa4fab4361a89bcbda3087d8714cddd
vazor222 <vazor222@gmail.com>
2015-10-10 10:00:56 -0600

    minor project cleanup

--- Assets/Scripts/StartGameListener.cs
+++ Assets/Scripts/StartGameListener.cs
@@ -4,24 +4,25 @@ using System.Collections;
 public class StartGameListener : MonoBehaviour
 {
 
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start()
+	{
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 
-    }
+	}
 
-    void OnGUI()
-    {
-        Event e = Event.current;
-        if (e.isKey && e.keyCode == KeyCode.W)
-        {
-            Application.LoadLevel("Main");
-        }
-    }
+	void OnGUI()
+	{
+		Event e = Event.current;
+		if (e.isKey && e.keyCode == KeyCode.W)
+		{
+			Application.LoadLevel("Main");
+		}
+
+	}
 }
