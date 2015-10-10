ca599bba8ebeafa0ac2f8656415ed91e81d4da22
vazor222 <vazor222@gmail.com>
2015-10-10 17:55:55 -0600

    a few wish updates

--- Assets/Scripts/Wish.cs
+++ Assets/Scripts/Wish.cs
@@ -35,9 +35,7 @@ public class Wish : MonoBehaviour {
 		"I long for the old days...",
 		"I miss my friends.  I want to see them again.",
 		"I wish for my health back!",
-		"I wish I had started a band.",
-		"I wish to quit my job!",
-		"I just don't want to be lonely anymore.",
+		"I wish I had started a band instead of getting a job!",
 		"I miss ma's cookin'...",
 		"I wish I could see my high school sweetheart again...",
 		"I wish to be remembered..."
@@ -48,7 +46,7 @@ public class Wish : MonoBehaviour {
 		"I wish for a nice house with a nice kitchen!",
 		"I wish for three more wishes!!!  Make that unlimited wishes!",
 		"I wish to win the game on Friday.",
-		"I wish I was better looking.  You know, taller, skinnier...",
+		"I wish I was better looking.",
 		"I wish I had more meaning in life.",
 		"I wish to be famous!",
 		"I wish for that pair of golden shoes at the mall.",
@@ -57,7 +55,7 @@ public class Wish : MonoBehaviour {
 	};
 	private static string[] revengeWishes =
 	{
-		"I was she was never born!",
+		"I wish she was never born!",
 		"I wish he were dead!",
 		"I wish my roommate would turn into a toad!",
 		"I wish to get even!"
@@ -67,16 +65,18 @@ public class Wish : MonoBehaviour {
 		"I wish for world peace!",
 		"I wish this day will never end!",
 		"I wish to meet someone with a sense of humor, like my dad!",
+		"I wish my parents were together again.",
 		"I wish for a box of chocolates.",
 		"I wish to spend more time with my family!!",
-		"I wish for a new puppy!"
+		"I really, really want a pet puppy!"
 	};
 	private static string[] learningWishes =
 	{
 		"I wish to learn a new language so I can travel the world!",
 		"I wish I could sing or play an instrument!",
-		"I wish I had more confidence!",
+		"I wish I had a heart! No, a brain! Maybe courage?",
 		"I wish to go to a great university!",
+		"I wish to quit my job!",
 		"I wish to inspire someone today.",
 		"I wish I was better at math...",
 		"I wish I were a better artist!",
@@ -85,7 +85,7 @@ public class Wish : MonoBehaviour {
 	};
 	private static string[] loveWishes = // must be two or more, final wish is end game wish
 	{
-		"I wish my parents were together again.",
+		"I just don't want to be lonely anymore.",
 		"I wish to meet someone tall, dark, handsome...someone who loves me!",
 		"I wish for us to be Together Forever! (This seems familiar somehow...)"
 	};
