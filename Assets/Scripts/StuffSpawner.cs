86dd78938ac8d01e463739d4e069d57151cb4a22
jstephens <jstephens@asdfa.net>
2015-10-10 00:15:51 -0600

    Spawner dev

---
+++ Assets/Scripts/StuffSpawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Universe))]
+public class StuffSpawner : MonoBehaviour {
+	public Vector3 size = new Vector3(500, 500, 500);
+
+	public GameObject wishTemplate;
+	public GameObject portalTemplate;
+
+	public float wishFrequency = .2f;
+	public float portalFrequency = .2f;
+
+	public Collider destructionBlock;
+
+	protected void Start() {
+		StartCoroutine(PortalSpawnner());
+		StartCoroutine(WishSpawnner());
+	}
+
+	protected void Update() {
+		
+	}
+
+	private IEnumerator PortalSpawnner() {
+		while (true) {
+			yield return new WaitForSeconds(portalFrequency);
+
+			var pObj = GameObject.Instantiate(portalTemplate);
+			pObj.transform.position = new Vector3(Random.Range(-size.x, size.x), -size.y, Random.Range(-size.z, size.z));
+		}
+	}
+
+	private IEnumerator WishSpawnner() {
+		while (true) {
+			yield return new WaitForSeconds(wishFrequency);
+
+			var pObj = GameObject.Instantiate(wishTemplate);
+			pObj.transform.position = new Vector3(Random.Range(-size.x, size.x), -size.y, Random.Range(-size.z, size.z));
+		}
+	}
+}
--- README.md
+++ README.md
@@ -13,3 +13,8 @@ Intro
 Main
   -the wells and portals, main falling action
 
+
+
+Deps/related
+	Spaceboxes: http://alexcpeterson.com/spacescape/
+	ZFPortals folder contains a personal library asset from sirbrialliance and is not comitted in this repo
