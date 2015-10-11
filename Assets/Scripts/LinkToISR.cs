14c10b48fca41c3dcfc6c15789798b56dcfd3b7a
vazor222 <vazor222@gmail.com>
2015-10-10 22:12:23 -0600

    updated player settings, made link to isr open in new window

--- Assets/Scripts/LinkToISR.cs
+++ Assets/Scripts/LinkToISR.cs
@@ -1,37 +1,36 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
 using System.Collections.Generic;
 
-public class LinkToISR : MonoBehaviour {
+public class LinkToISR : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (EventSystem.current.IsPointerOverGameObject())
-            {
-                var pd = new PointerEventData(EventSystem.current) { position = Input.mousePosition };
-                var results = new List<RaycastResult>();
-                EventSystem.current.RaycastAll(pd, results);
-                foreach (var result in results)
-                {
-                    if (result.gameObject.name == "ISRLink")
-                    {
-                        Application.OpenURL("http://www.indiespeedrun.com");
-                    }
-                }
-            }
-        }
-    }
+	void Start()
+	{
 
-    void OnMouseDown ()
-    {
-        Debug.Log("reall?");
-    }
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (EventSystem.current.IsPointerOverGameObject())
+			{
+				var pd = new PointerEventData(EventSystem.current) { position = Input.mousePosition };
+				var results = new List<RaycastResult>();
+				EventSystem.current.RaycastAll(pd, results);
+				foreach (var result in results)
+				{
+					if (result.gameObject.name == "ISRLink")
+					{
+						//Application.OpenURL("http://www.indiespeedrun.com");
+						Application.ExternalEval("window.open('http://www.indiespeedrun.com','IndieSpeedRun')");
+					}
+				}
+			}
+		}
+	}
 }
