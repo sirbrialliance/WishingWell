3b6342a794eb47f8792c56be4b07d5ddf643e16b
vazor222 <vazor222@gmail.com>
2015-10-10 21:40:45 -0600

    wishes glow and texture offset animate, skybox glowy orange

---
+++ Assets/Scripts/MaterialAnimator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialAnimator : MonoBehaviour {
+
+	public Material animMaterial;
+	public float animSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2(animSpeed*Time.realtimeSinceStartup, 0);
+		animMaterial.SetTextureOffset("_BumpMap", offset);
+		animMaterial.SetTextureOffset("_MainTex", offset);
+	}
+}
